058d49dc66fb56b430f2a85504101267831ad6a0
Abdullah Al  Muti <abdullah.muti@streamstech.com>
2024-08-13 15:47:26 +0600

    added

--- src/editor.rs
+++ src/editor.rs
@@ -1,36 +1,64 @@
-use std::io::{self, Read};
-use crossterm::event;
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
-use crossterm::event::{read, Event::Key, KeyCode::Char};
+use crossterm::event::{read, Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
+use crossterm::execute;
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType};
+use std::io::stdout;
 
-pub struct Editor{
-
+pub struct Editor {
+    should_quit: bool,
 }
 
 impl Editor {
     pub fn default() -> Self {
-        Editor{}
+        Editor { should_quit: false }
+    }
+    pub fn run(&mut self) {
+        Self::initialize().unwrap();
+        let result = self.repl();
+        Self::terminate().unwrap();
+        result.unwrap();
     }
 
-    pub fn run(&self){
-        enable_raw_mode().unwrap();
+    fn initialize() -> Result<(), std::io::Error> {
+        enable_raw_mode()?;
+        Self::clear_screen()
+    }
+    fn terminate() -> Result<(), std::io::Error> {
+        disable_raw_mode()
+    }
+    fn clear_screen() -> Result<(), std::io::Error> {
+        let mut stdout = stdout();
+        execute!(stdout, Clear(ClearType::All))
+    }
+
+    fn repl(&mut self) -> Result<(), std::io::Error> {
         loop {
-            match read() {
-                Ok(Key(event)) => {
-                    println!("{:?} \r", event);
-                    match (event.code) {
-                        Char(c) => {
-                            if c == 'q' {
-                                break;
-                            }
-                        },
-                        _ => (),
-                    }
-                },
-                Err(err) => println!("Error: {}", err),
+            let event = read()?;
+            self.evaluate_event(&event);
+            self.refresh_screen()?;
+            if self.should_quit {
+                break;
+            }
+        }
+        Ok(())
+    }
+    fn evaluate_event(&mut self, event: &Event) {
+        if let Key(KeyEvent {
+            code, modifiers, ..
+        }) = event
+        {
+            match code {
+                Char('q') if *modifiers == KeyModifiers::CONTROL => {
+                    self.should_quit = true;
+                }
                 _ => (),
             }
         }
-        disable_raw_mode().unwrap();
+    }
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        if self.should_quit {
+            Self::clear_screen()?;
+            print!("Goodbye.\r\n");
+        }
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,5 @@ use editor::Editor;
 
 
 fn main() {
-    let editor = Editor::default();
-    editor.run();
+    Editor::default().run();
 }
